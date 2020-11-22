54625918f353f43b5d14074ce09dd33ab79239c2
saranshgupta27 <43762009+saranshgupta27@users.noreply.github.com>
2020-11-22 19:17:44 +0530

    Update main.js

--- main.js
+++ main.js
@@ -11,30 +11,72 @@ let ok = "D";
 let nk = "D";
 let i = 0;
 document.querySelector("#t1").addEventListener("keydown", function (e) {
-  
-  if (e.key.toUpperCase()!= 'backspace') {
-    if (i < 13) 
-    {
-      nk = e.key;
-      console.log(ok, nk);
-      if (isNaN(nk) != isNaN(ok)) 
-      {
-        console.log("inside");
-        e.target.value =e.target.value.substr(0, i)+" ";
+  setTimeout(function(){ 
+  //   temp=e.target.value;
+  //   console.log(temp.length);
+  //   i=0;j=0;
+  //   while(i<temp.length)
+  // {
+  //   j=i+1;
+  //   if (isNaN(temp[i]) && !isNaN(temp[j]) )
+  //   {
       
-        i++;
+  //       // if(temp[j]=='')
+  //       // {
+  //       //   j=j+1
+  //       // }
+  //       swap=temp[j];
+  //       temp[j]=' ';
+  //       temp[j+1]=swap;
+  //       break;
+  //     }
+  //     else 
+  //     { continue};
+  
+  //   }i++;
+  if (i < 13) 
+      {
+        nk = e.key;
+        console.log(ok, nk);
+        if (isNaN(nk) != isNaN(ok)) 
+        {
+          console.log("inside");
+          e.target.value =e.target.value.substr(0, i)+" ";
+        
+          i++;
+        }
+        ok = nk;
       }
+      i++;
       ok = nk;
-    }
-    i++;
-    ok = nk;
-  } else{ nk=ok;
-    if(ok!=' ' && e.target.value.substr(i-1)==' ')
-    {
-      ok=e.target.value.substr(i-2);i--;
-    } 
-      else {ok=e.target.value.substr(i-1); i--; console.log(ok, nk); }}
+    
+   }, 500);
+  
 });
+  
+//   if (e.key.toUpperCase()!= 'backspace') {
+//     if (i < 13) 
+//     {
+//       nk = e.key;
+//       console.log(ok, nk);
+//       if (isNaN(nk) != isNaN(ok)) 
+//       {
+//         console.log("inside");
+//         e.target.value =e.target.value.substr(0, i)+" ";
+      
+//         i++;
+//       }
+//       ok = nk;
+//     }
+//     i++;
+//     ok = nk;
+//   } else{ nk=ok;
+//     if(ok!=' ' && e.target.value.substr(i-1)==' ')
+//     {
+//       ok=e.target.value.substr(i-2);i--;
+//     } 
+//       else {ok=e.target.value.substr(i-1); i--; console.log(ok, nk); }}
+// });
 //   let i=0
 //   while(i<13){
 //   nb=e.target.value;
