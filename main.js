9cadbcb6c19cee270dc71863413a27456dbe6d03
Shivam <shivamsn97@gmail.com>
2020-12-14 10:33:53 +0530

    Changed seperator to space instead of dash. Also some other minor fix in mask,

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var selector = document.getElementById("t1");
 Inputmask({
-  mask: "aa-9{1,2}-a{1,3}-9999",
-  placeholder: "MH-14-DH-56XX",
+  mask: "aa 9{1,2} [a{1,3} ]9999",
+  placeholder: "▯",
   definitions: {
     '*': {
       casing: "upper"
