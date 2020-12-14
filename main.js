4732bcc49e274294f6b546b94cdb27a3850d0afa
Shivam <shivamsn97@gmail.com>
2020-12-14 18:41:04 +0530

    Empty placeholder on input value enter.

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 var selector = document.getElementById("t1");
+
 Inputmask({
   mask: "aa 9{1,2} [a{1,3} ]9999",
-  placeholder: "○",
+  placeholder: "",
   definitions: {
     '*': {
       casing: "upper"
