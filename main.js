5373fb8196f2e1d80b145ba1a4cccfdf94225075
Shivam <shivamsn97@gmail.com>
2020-12-14 11:17:29 +0530

    changed placeholder to ○ It lookes better then previous

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var selector = document.getElementById("t1");
 Inputmask({
   mask: "aa 9{1,2} [a{1,3} ]9999",
-  placeholder: "▯",
+  placeholder: "○",
   definitions: {
     '*': {
       casing: "upper"
