61d6eb4cc5fd933e61206bf1eefd099bb6fc2a4d
saranshgupta27 <43762009+saranshgupta27@users.noreply.github.com>
2021-04-14 03:35:59 +0530

    Added Fast Tag Information and Driving License Information

--- main.js
+++ main.js
@@ -1,48 +1,89 @@
-let data = document.querySelector("#t1");
-let a=0;
-function send() {
-  let no = document.querySelector("#t1").value;
-  console.log(no);
-  
+let allow = true;
+let allow_2 = false;
+
+function licenseplate_y() {
+  allow = true;
+  allow_2 = false;
+  document.querySelector("#d1").innerHTML =
+    '<input type="text" oninput="this.value = this.value.toUpperCase()" placeholder="MH 14 DH 56XX" name="name" maxlength="14" id="t1" pattern="^[A-Z]{2} [0-9]{1,2} [A-Z]{0,3} [0-9]{4}$" required />';
+  text_format();
+  document.querySelector("#h1-2").textContent =
+    "Enter Vehicle Registration Number";
 }
 
+function licenseplate_n() {
+  allow = false;
+  allow_2 = true;
+  document.querySelector("#d1").innerHTML =
+    '<input type="text" oninput="this.value = this.value.toUpperCase()" placeholder="DL 08123456XXXXX" name="name"  maxlength="16 "id="t1" class="t2" pattern="^[A-Z]{2} [0-9]{12,13}$" required />';
+  text_format();
 
-
-let size=0;
-document.querySelector("#t1").addEventListener("keydown", function (e) {
-
-setTimeout(function(){ 
-  
-  if(e.target.value.length >=size ){   
-    
-    if (e.target.value.length == "2" ) {
-     
-    e.target.value = e.target.value + " ";
-    
+  document.querySelector("#h1-2").textContent = "Enter Driving License Number";
+  if (allow_2) {
+    document
+      .querySelector("#t1")
+      .addEventListener("keydown", function fdata(e) {
+        setTimeout(function () {
+          if (e.target.value.length == "2") {
+            e.target.value = e.target.value + " ";
+          }
+        }, 200);
+      });
   }
-  if (e.target.value.length == "5" && isNaN(e.target.value[4]) && e.target.value[4]!=' '  ) {
-    e.target.value = e.target.value.substr(0,4) + " "+e.target.value[4];
-    
-  }
-  else if(e.target.value.length == "5" &&  e.target.value[4]!=' ') {
-    e.target.value = e.target.value + " ";
-    
-  }
-  if(e.target.value.length=="7" && !isNaN(e.target.value[e.target.value.length-1])){
-    e.target.value = e.target.value.substr(0,6) + " "+e.target.value[6];
-   }
-  if(e.target.value.length >6 && !isNaN(e.target.value[e.target.value.length-1])  && e.target.value[e.target.value.length-1]!=' ' && isNaN(e.target.value[e.target.value.length-2]))
-  { 
-   console.log('length'+e.target.value.length);
-   console.log('value-1 '+e.target.value[e.target.value.length-1]);
-    swap=e.target.value[e.target.value.length-1];
-    console.log('swap '+swap);
-    e.target.value=  e.target.value.substr(0,e.target.value.length-1) + " "+`${swap}`;
+}
 
+function text_format() {
+  if (allow) {
+    let size = 0;
+    let swap;
+    document
+      .querySelector("#t1")
+      .addEventListener("keydown", function fdata(e) {
+        setTimeout(function () {
+          if (e.target.value.length >= size) {
+            if (e.target.value.length == "2") {
+              e.target.value = e.target.value + " ";
+            }
+            if (
+              e.target.value.length == "5" &&
+              isNaN(e.target.value[4]) &&
+              e.target.value[4] != " "
+            ) {
+              e.target.value =
+                e.target.value.substr(0, 4) + " " + e.target.value[4];
+            } else if (
+              e.target.value.length == "5" &&
+              e.target.value[4] != " "
+            ) {
+              e.target.value = e.target.value + " ";
+            }
+            if (
+              e.target.value.length == "7" &&
+              !isNaN(e.target.value[e.target.value.length - 1])
+            ) {
+              e.target.value =
+                e.target.value.substr(0, 6) + " " + e.target.value[6];
+            }
+            if (
+              e.target.value.length > 6 &&
+              !isNaN(e.target.value[e.target.value.length - 1]) &&
+              e.target.value[e.target.value.length - 1] != " " &&
+              isNaN(e.target.value[e.target.value.length - 2])
+            ) {
+              // console.log("length" + e.target.value.length);
+              // console.log("value-1 " + e.target.value[e.target.value.length - 1]);
+              swap = e.target.value[e.target.value.length - 1];
+              console.log("swap " + swap);
+              e.target.value =
+                e.target.value.substr(0, e.target.value.length - 1) +
+                " " +
+                `${swap}`;
+            }
+          }
+          size = e.target.value.length;
+        }, 90);
+      });
   }
- } size=e.target.value.length;
-},90); 
-    
+}
 
-});
-  
+text_format();
--- main2.js
+++ main2.js
@@ -1,16 +1,22 @@
 let hehe = window.location.href;
 let a = hehe.split("?")[1].split("=")[1].split("+");
+console.log(a);
+const category = `${hehe.split("&")[1].split("=")[1]}`;
+if (category != "dl_info") {
+  a = `${a[0]} ${a[1]} ${a[2]} ${a[3].split("&")[0]}`;
+  document.querySelector("#np").textContent = `${a}`;
+  a = a.replaceAll(" ", "");
+} else {
+  a = `${a[0]} ${a[1].split("&")[0]}`;
+  document.querySelector("#np").textContent = `${a}`;
+}
+console.log(a);
 
-a = `${a[0]} ${a[1]} ${a[2]} ${a[3]}`;
-
-// let a='DLK23';
-document.querySelector("#np").textContent = `${a}`;
-
-function get_output(a) {
-  fetch(`https://apiclub.in/api/v1/vehicle_info/${a.replaceAll(" ", "")}`, {
+function call_api(a, category) {
+  fetch(`https://apiclub.in/api/v1/${category}/${a}`, {
     method: "POST",
     headers: {
-      "API-Key": "89a61b67b23117a4ac60dd480f80e674",
+      "API-Key": "941b667a02ec4c8a0a72af01bb451c50",
     },
   })
     .then((response) => {
@@ -36,21 +42,66 @@ const handleErrors = () => {
 
   document.querySelector(".div5").style.display = "flex";
 };
+
 function set_html(y) {
   console.log(y);
-  let b = document.querySelectorAll(".lb");
+  const b = document.querySelectorAll(".lb");
+  const d = document.querySelectorAll(".img-caption");
+  const img = document.querySelectorAll(".grid-img");
   document.querySelector(".div4").style.display = "none";
   document.querySelector(".div3").style.display = "flex";
-
-  b[0].textContent = y.owner_name;
-  b[1].textContent = y.fitness_upto;
-  b[2].textContent = y.fuel_type;
-  b[3].textContent = y.model;
-  b[4].textContent = y.insurance_expiry;
-  b[5].textContent = y.engine_number;
-  b[6].textContent = y.chassis_number;
-  b[7].textContent = y.registration_date;
-  b[8].textContent = y.rto;
+  if (category == "vehicle_info") {
+    img.forEach((item) => {
+      item.style.visibility = "visible";
+    });
+    b[0].textContent = y.owner_name;
+    b[1].textContent = y.fitness_upto;
+    b[2].textContent = y.fuel_type;
+    b[3].textContent = `${y.brand_name} ${y.brand_model}`;
+    b[4].textContent = y.insurance_expiry;
+    b[5].textContent = y.engine_number;
+    b[6].textContent = y.chassis_number;
+    b[7].textContent = y.registration_date;
+    b[8].textContent = y.rto;
+  } else if (category == "dl_info") {
+    d[0].textContent = "Status";
+    b[0].textContent = y.status;
+    d[1].textContent = "RTO Code";
+    b[1].textContent = y.rto_code;
+    d[2].textContent = "Date Of Birth";
+    b[2].textContent = y.dob;
+    d[3].textContent = "Vehicle Class";
+    b[3].textContent = y.vehicle_class;
+    d[4].textContent = "Holder Name";
+    b[4].textContent = y.holder_name;
+    d[5].textContent = "Gender";
+    b[5].textContent = y.gender;
+    d[6].textContent = "RTO";
+    b[6].textContent = y.rto;
+    d[7].textContent = "Valid From";
+    b[7].textContent = y.valid_from;
+    d[8].textContent = "Valid Upto";
+    b[8].textContent = y.valid_upto;
+  } else if (category == "fast_tag_info") {
+    d[0].textContent = "Status";
+    b[0].textContent = y.isValid;
+    d[1].textContent = "Owner Name";
+    b[1].textContent = y.owner_name;
+    d[2].textContent = "Biller ID";
+    b[2].textContent = y.biller_id;
+    d[3].textContent = "Fast tag Balance";
+    b[3].textContent = y.fastag_balance;
+    d[4].textContent = "Issuer name";
+    b[4].textContent = y.issuer_name;
+    d[5].textContent = "Min Amount";
+    b[5].textContent = y.min_amount;
+    d[6].textContent = "Max Amount";
+    b[6].textContent = y.max_amount;
+    d[7].textContent = "";
+    b[7].textContent = "";
+    d[8].textContent = "";
+    b[8].textContent = "";
+  }
 }
 
-get_output(a);
+call_api(a, category);
